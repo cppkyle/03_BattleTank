bd3a686f5caa3a8bb96393f0540dcef69d3ca8ce
cppkyle <33187539+cppkyle@users.noreply.github.com>
2018-08-03 22:46:42 -0400

    All previous bugs fixed. Added C++ classes for PlayerTankController and Tank. Pointer defense active.

---
+++ BattleTank 4.20/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay() {
+
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("TankPlayerCharacter Begin Play"));
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Error, TEXT("%s does not control anything!"), *this->GetName());
+	} else {
+		UE_LOG(LogTemp, Warning, TEXT("%s is controlling %s"), *this->GetName(), *ControlledTank->GetName());
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const {
+
+	return Cast<ATank>(GetPawn());
+}
+
